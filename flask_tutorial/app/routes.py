236997f4ee3646fdd7211348960be615c3eb1dc3
Dennis Nguyen-Do <dennisnguyendo@gmail.com>
2019-07-04 13:48:04 -0400

    black format scripts

--- flask_tutorial/app/routes.py
+++ flask_tutorial/app/routes.py
@@ -2,11 +2,15 @@ from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
-@app.route('/login', methods=["GET", "POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash("Login requested for user {}, remember me={}".format(
-			form.username.data, form.remember_me.data))
-		return redirect(url_for("index"))
-	return render_template("login.html", title="Sign In", form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            "Login requested for user {}, remember me={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
