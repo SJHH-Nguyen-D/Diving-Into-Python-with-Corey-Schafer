e244d2f955a909e5c68ffb522f0a8de2f482a71f
Dennis Nguyen-Do <dennisnguyendo@gmail.com>
2019-07-07 10:31:26 -0400

    add avatar to profile

--- flask_tutorial/app/forms.py
+++ flask_tutorial/app/forms.py
@@ -11,23 +11,30 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
 
+
 class RegistrationForm(FlaskForm):
-	""" Form class for registration of user account """
+    """ Form class for registration of user account """
 
-	username = StringField("Username", validators=[DataRequired()])
-	email = StringField("Email", validators=[DataRequired(), Email()])
-	password = PasswordField("Password", validators=[DataRequired()])
-	password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
-	submit = SubmitField("Register")
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField(
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Register")
 
-	def validate_username(self, username):
-		""" if username already in database, raise error """
-		user = User.query.filter_by(username=username.data).first()
-		if user is not None:
-			raise ValidationError("Username already exists. Please try a different username.")
+    def validate_username(self, username):
+        """ if username already in database, raise error """
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(
+                "Username already exists. Please try a different username."
+            )
 
-	def validate_email(self, email):
-		""" if email already in database, raise error 	"""
-		user = User.query.filter_by(email=email.data).first()
-		if user is not None:
-			raise ValidationError("This email has already been registered. Please try another email address.")
+    def validate_email(self, email):
+        """ if email already in database, raise error 	"""
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(
+                "This email has already been registered. Please try another email address."
+            )
--- flask_tutorial/app/models.py
+++ flask_tutorial/app/models.py
@@ -3,10 +3,13 @@ from app import login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
+
 
 @login.user_loader
 def load_user(id):
-	return User.query.get(int(id))
+    return User.query.get(int(id))
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,16 +22,23 @@ class User(UserMixin, db.Model):
         return "<User {}>".format(self.username)
 
     def set_password(self, password):
-    	self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-    	return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size=128):
+        """ generate avatar for user profile """
+
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return "https://wwww.gravatar.com/avatar/{}?s=identicon&s={}".format(
+            digest, size)
 
 class Post(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	body = db.Column(db.String(140))
-	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-	user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
-	def __repr__(self):
-		return "<Post {}>".format(self.body)
+    def __repr__(self):
+        return "<Post {}>".format(self.body)
