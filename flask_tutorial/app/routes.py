a5da9d750d90055b422cef4954ff18e4c80bb4fd
Dennis Nguyen-Do <dennisnguyendo@gmail.com>
2019-07-08 12:17:24 -0400

    completed profile page and avatars section tutorial

--- flask_tutorial/app/routes.py
+++ flask_tutorial/app/routes.py
@@ -113,15 +113,19 @@ def before_request():
 @app.route("/edit_profile", methods=["GET", "POST"])
 @login_required
 def edit_profile():
-    """ Routes users to the edit profile form view """
+    """ Edit Profile view function: Routes users to the edit profile form view """
 
     form = EditProfileForm()
+    # if the  form is validated on submission, 
     if form.validate_on_submit():
+        # copy the form into the User database object
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
+        # write the changes of the profile edit into the database
         db.session.commit()
         flash("Your changes have been saved")
         return redirect(url_for("edit_profile"))
+    # validate_on_submit() returns false if request method="GET"
     elif request.method == "GET":
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
