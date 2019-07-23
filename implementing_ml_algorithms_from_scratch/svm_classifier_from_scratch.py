9a99824327e2d613d47f5e60f6581440c17e8503
Dennis Nguyen-Do <dennisnguyendo@gmail.com>
2019-07-22 23:39:11 -0400

    start on svm_from_scratch

--- implementing_ml_algorithms_from_scratch/svm_classifier_from_scratch.py
+++ implementing_ml_algorithms_from_scratch/svm_classifier_from_scratch.py
@@ -4,14 +4,34 @@
 
 import math
 import numpy as np 
+import pandas as pd 
 from sklearn import datasets
-from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt 
+from matplotlib import style
+style.use("ggplot")
 
 # load in iris dataset for examples
 wine = datasets.load_wine()
-X = wine.data
-y = wine.target
+wine = pd.DataFrame(np.c_[wine["data"], wine["target"]],
+	columns=wine["feature_names"]+["target"])
+X = wine[wine.columns[wine.columns != "target"]].values
+y = wine[wine.columns[wine.columns == "target"]].values
 
+print(wine.head())
 
-def svm_classification():
-    pass
+class Support_Vector_Machine:
+	def __init__(self, C=0.01, visualization=True):
+		self.visualization = visualization
+		self.colors {1: "r", -1: "b"}
+		if self.visualization:
+			self.fig = plt.figure()
+			self.ax = plt.fig.add_subplot(1, 1, 1)
+
+	def fit(self, X, y):
+		self.w = 
+		self.b = 
+
+	def predict(self, X):
+		# sign(x dot w+b)
+		y_pred = np.sign(np.dot(np.array(X), self.w) + self.b)
+		return y_pred
