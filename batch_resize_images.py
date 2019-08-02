1929efdeee66c389e2a233dfdb2f9a81fa756d41
Dennis Nguyen-Do <dennisnguyendo@gmail.com>
2019-08-02 12:00:19 -0400

    change dir for image resizing

--- batch_resize_images.py
+++ batch_resize_images.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os, sys
 
-path = "/home/dennis/Downloads/knight_sprites/png"
+path = "/home/dennis/Desktop/Link to datascience_job_portfolio/images_to_resize_for_blog"
 
 
 def resize(directory):
@@ -10,8 +10,9 @@ def resize(directory):
         if os.path.isfile(os.path.join(path, item)):
             im = Image.open(os.path.join(path, item))
             f, e = os.path.splitext(os.path.join(path, item))
-            imResize = im.resize((500, 500), Image.ANTIALIAS)
-            imResize.save(f + " resized.png", "PNG", quality=90)
+            imResize = im.resize((1038, 437), Image.ANTIALIAS)
+            imResize.save(f + " resized.jpeg", "JPEG", quality=100)
+    print("Finished Resizing!!!")
 
 
 if __name__ == "__main__":
